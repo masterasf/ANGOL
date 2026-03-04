42dc012a87f54638c1726946c082439addae8dbf
masterasf_ <65750924+masterasf@users.noreply.github.com>
2026-03-04 11:59:52 +0100

    Add files via upload

---
+++ angielski.js
@@ -0,0 +1,177 @@
+window.VOCAB_SETS = window.VOCAB_SETS || {};
+
+window.VOCAB_SETS["angielski-umiejetnosci"] = {
+  id: "angielski-umiejetnosci",
+  title: "Nauka i umiejętności",
+  emoji: "📚",
+  description: "Wyrażenia związane z nauką, rozwojem i umiejętnościami",
+  color: "#ff6b6b",
+  words: [
+    ["attempt to do something","próba zrobienia czegoś"],
+    ["be a boost to someone's confidence","podnieść komuś samoocenę"],
+    ["be capable of doing sth","być zdolnym do zrobienia czegoś"],
+    ["be hopeless at","być beznadziejnym w"],
+    ["be motivated","być zmotywowanym"],
+    ["come naturally to someone","przychodzić komuś naturalnie"],
+    ["focus your attention on","skupić swoją uwagę na"],
+    ["fulfil a dream","spełnić marzenie"],
+    ["gain experience","zdobywać doświadczenie"],
+    ["get the feel of","wyczuć coś"],
+    ["go downhill","pogarszać się"],
+    ["have the potential to","mieć potencjał"],
+    ["inspire someone","zainspirować kogoś"],
+    ["make all the difference","zrobić różnicę"],
+    ["master","opanować"],
+    ["not getting anywhere","utknąć w miejscu"],
+    ["overcome a problem","przezwyciężyć problem"],
+    ["pick up the basics","nauczyć się podstaw"],
+    ["put the theory into practice","zastosować teorię w praktyce"],
+    ["that's always a plus","to zawsze plus"],
+    ["watch online tutorials","oglądać samouczki online"]
+  ]
+};
+
+window.VOCAB_SETS["angielski-phrasal"] = {
+  id: "angielski-phrasal",
+  title: "Phrasal Verbs",
+  emoji: "🔗",
+  description: "Popularne czasowniki frazowe",
+  color: "#ffd93d",
+  words: [
+    ["catch up on","nadrabiać"],
+    ["come across","natknąć się"],
+    ["drop out","rzucić"],
+    ["figure out","zrozumieć"],
+    ["go on to do sth","dalej robić coś"],
+    ["go through","zapoznać się z czymś dokładnie"],
+    ["live up to expectations","spełniać oczekiwania"],
+    ["pick up","podłapać umiejętność"],
+    ["get hold of sb","złapać kogoś"],
+    ["get into sth","zainteresować się czymś"],
+    ["get off work","wyjść z pracy"],
+    ["get out","wyjść"],
+    ["get someone","odebrać kogoś"],
+    ["get to know","dowiedzieć się"],
+    ["fall behind with","mieć zaległości"]
+  ]
+};
+
+window.VOCAB_SETS["angielski-pamiec"] = {
+  id: "angielski-pamiec",
+  title: "Nauka i pamięć",
+  emoji: "🧠",
+  description: "Słownictwo związane z przyswajaniem wiedzy i wiekiem",
+  color: "#6bcb77",
+  words: [
+    ["absorb information","przyswajać informacje"],
+    ["acquire a second language","opanować drugi język"],
+    ["adolescence","wiek dojrzewania"],
+    ["adulthood","dorosłość"],
+    ["be wise","być mądrym"],
+    ["distract","rozpraszać"],
+    ["have an extensive vocabulary","posiadać bogate słownictwo"],
+    ["in middle age","w średnim wieku"],
+    ["in your late teens","w wieku 18-19"],
+    ["mature","dojrzały"],
+    ["mental arithmetic","arytmetyka mentalna"],
+    ["recall people's names","przypominać sobie imiona ludzi"],
+    ["retirement","przejście na emeryturę"],
+    ["the older generation","starsze pokolenie"]
+  ]
+};
+
+window.VOCAB_SETS["angielski-emocje"] = {
+  id: "angielski-emocje",
+  title: "Emocje i relacje",
+  emoji: "❤️",
+  description: "Słownictwo o emocjach, relacjach i stanie umysłu",
+  color: "#e91e8c",
+  words: [
+    ["assertive","asertywny"],
+    ["be justified","być uzasadnionym"],
+    ["criticism","krytyka"],
+    ["judgment","ocena sytuacji"],
+    ["lose your temper","stracić panowanie"],
+    ["maintain relationship","utrzymywać relacje"],
+    ["perspective","perspektywa"],
+    ["state of mind","stan umysłu"]
+  ]
+};
+
+window.VOCAB_SETS["angielski-przysłowki"] = {
+  id: "angielski-przysłowki",
+  title: "Przysłówki i komentarze",
+  emoji: "💬",
+  description: "Przysłówki komentujące i wyrażenia dyskursu",
+  color: "#64ffda",
+  words: [
+    ["admittedly","wprawdzie"],
+    ["amazingly","zadziwiająco"],
+    ["basically","w zasadzie"],
+    ["hopefully","miejmy nadzieję"],
+    ["ideally","najlepiej by było"],
+    ["interestingly","co ciekawe"],
+    ["obviously","oczywiście"],
+    ["personally","osobiście"],
+    ["surprisingly","zaskakująco"],
+    ["unfortunately","niestety"]
+  ]
+};
+
+window.VOCAB_SETS["angielski-szkola"] = {
+  id: "angielski-szkola",
+  title: "Szkoła i przybory",
+  emoji: "🎒",
+  description: "Przybory szkolne, przedmioty i życie szkolne",
+  color: "#ff8c42",
+  words: [
+    ["be expelled","zostać wydalonym"],
+    ["be off school","być poza szkołą"],
+    ["be tutored","brać korepetycje"],
+    ["citizenship","obywatelstwo"],
+    ["correction tape","korektor w taśmie"],
+    ["clipboard","podkładka z klipsem"],
+    ["compass","cyrkiel"],
+    ["cram","wkuwać"],
+    ["design and technology","technika"],
+    ["drawing pins","pinezki"],
+    ["fall behind with","mieć zaległości"],
+    ["fineliner pen","cienkopis"],
+    ["hole punch","dziurkacz"],
+    ["humanities","nauki humanistyczne"],
+    ["philosophy","filozofia"],
+    ["protractor","kątomierz"],
+    ["ring binder","segregator"],
+    ["set square","ekierka"],
+    ["skip school","opuszczać lekcje"],
+    ["stapler","zszywacz"],
+    ["STEM","przedmioty ścisłe"],
+    ["social studies","studia społeczne"]
+  ]
+};
+
+window.VOCAB_SETS["angielski-tozsamosc"] = {
+  id: "angielski-tozsamosc",
+  title: "Tożsamość i rozwój",
+  emoji: "🌱",
+  description: "Tożsamość, samorozwój i cechy charakteru",
+  color: "#7c3aed",
+  words: [
+    ["be natural","mieć wrodzone zdolności"],
+    ["be your real self","bądź prawdziwym sobą"],
+    ["build rapport","budować relacje"],
+    ["capitalise on sth","skorzystać z czegoś"],
+    ["cultural identity","tożsamość kulturowa"],
+    ["fulfil your potential","wykorzystać swój potencjał"],
+    ["have strict self-control","mieć dobrą samokontrolę"],
+    ["identify with","identyfikować się z"],
+    ["multicultural","wielokulturowy"],
+    ["push your boundaries","przekraczaj swoje granice"],
+    ["self-esteem","poczucie własnej wartości"],
+    ["sense of belonging","poczucie przynależności"],
+    ["show leadership","wykazywać cechy przywódcze"],
+    ["social status","status społeczny"],
+    ["superior","lepszy ponad przeciętny"],
+    ["think outside the box","myśleć nieszablonowo"]
+  ]
+};
